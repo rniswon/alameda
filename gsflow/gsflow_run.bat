d054eaf640a04195dc5ef2b10bb6818de825bc29
GEOG-LARSEN-WIN\saalem.adera <saalem.adera@gmail.com>
2018-01-19 15:50:00 -0800

    added files to .gitignore file, updated run file to grab latest version of gsflow model

--- gsflow/gsflow_run.bat
+++ gsflow/gsflow_run.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-.\bin\gsflow .\alameda_gsflow.control 
+.\bin\gsflow_1.2.2beta .\alameda_gsflow.control 
 ECHO.
 ECHO Run complete. Please press enter when you want to continue.
 PAUSE>NUL
