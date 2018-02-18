4d1b8c5491e08912d096fbb197c5b9faea74b3bf
GS\emorway-usgs <emorway@usgs.gov>
2018-02-18 06:25:45 -0800

    direct UZF package to use adjusted_finf.txt instead of finf.txt, where adjusted_finf.txt is adjusted by a scaling array manipulated by PEST

--- gsflow/_RunModel.bat
+++ gsflow/_RunModel.bat
@@ -5,6 +5,12 @@ cd    .\INPUT\modflow_all\pest\
 call  00_MF_Call_Array_Build.bat
 cd    ..\..\..\
 
+REM  Scale finf by subasin (original finf array multiplied by scaling array)
+cd    .\INPUT\modflow_all\uzf_support\
+call  twoarray.exe    < twoarray.in
+call  ReformARRAY.exe < RefArr_In_finf.txt
+cd    ..\..\..\
+
 REM  Call model runfile
 MF_NWT.exe alameda_all.nam
 
