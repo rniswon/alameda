9f782a19eec40f6aa5da035972b8f77ffa3b909d
GS\emorway-usgs <emorway@usgs.gov>
2018-03-13 09:29:34 -0700

    Fixing some mistakes, moved .sfr.tpl file, removed missing Streambed parameters from control file

--- gsflow/_RunModel.bat
+++ gsflow/_RunModel.bat
@@ -9,6 +9,9 @@ REM  Reformat the FINF scaling array from a single column to array format
 cd    .\INPUT\modflow_all\uzf_support\
 call  ReformARRAY.exe < RefArr_In_scale_finf.txt
 
+REM  Also, reformat the surfK array from a single column to array format
+call  ReformARRAY.exe < RefArr_In_surfK.txt
+
 REM  Scale finf by subasin (original finf array multiplied by scaling array)
 call  twoarray.exe    < twoarray.in
 call  ReformARRAY.exe < RefArr_In_finf.txt
