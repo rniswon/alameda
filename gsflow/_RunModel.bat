f04ae745243efeb284d4107f7b95cf71b1da426f
GS\emorway-usgs <emorway@usgs.gov>
2018-04-25 07:24:09 -0700

    Need to reformat new pET scaling array to work with twoarray.exre

--- gsflow/_RunModel.bat
+++ gsflow/_RunModel.bat
@@ -5,17 +5,24 @@ cd    .\INPUT\modflow_all\pest\
 call  00_MF_Call_Array_Build.bat
 cd    ..\..\..\
 
-REM  Reformat the FINF scaling array from a single column to array format
+REM  Reformat the FINF & pET scaling arrays from a single column to array format
 cd    .\INPUT\modflow_all\uzf_support\
 call  ReformARRAY.exe < RefArr_In_scale_finf.txt
+call  ReformARRAY.exe < RefArr_In_scale_pET.txt
 
 REM  Also, reformat the surfK array from a single column to array format
 call  ReformARRAY.exe < RefArr_In_surfK.txt
 
 REM  Scale finf by subasin (original finf array multiplied by scaling array)
-call  twoarray.exe    < twoarray.in
+call  twoarray.exe    < twoarray_finf.in
 call  ReformARRAY.exe < RefArr_In_finf.txt
 cd    ..\..\..\
+
+REM  Scale pET by subasin (original pET array multiplied by scaling array)
+call  twoarray.exe    < twoarray_pET.in
+call  ReformARRAY.exe < RefArr_In_pET.txt
+cd    ..\..\..\
+
 
 REM  Call model runfile
 MF_NWT.exe alameda_all.nam
