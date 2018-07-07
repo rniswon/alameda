15af3510d43d2a7a906dadd3c134bf4b11c51763
GEOG-LARSEN-WIN\saalem.adera <saalem.adera@gmail.com>
2018-07-06 17:21:51 -0700

    fixed some file path errors

--- gsflow/_RunModel.bat
+++ gsflow/_RunModel.bat
@@ -23,10 +23,10 @@ call  ReformARRAY.exe < RefArr_In_pET.txt
 cd    ..\..\..\
 
 REM  Call model runfile
-MF_NWT.exe alameda_lower_modflow.nam
+MF_NWT.exe alameda_lower.nam
 
 REM  Perform post-processing routine #1
-cd    .\OUTPUT\modflow\subbasin_runoff\
+cd    .\output\modflow\subbasin_runoff\
 call  get_runoff.exe
 cd    ..\
 
