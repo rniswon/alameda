ace64d7e427ec2605ec0b4b94077bae0670df585
GEOG-LARSEN-WIN\saalem.adera <saalem.adera@gmail.com>
2019-02-23 13:35:26 -0800

    one additional change to _RunModel_tr.bat file - removed the subbasin runoff calculations because no longer needed with transient model (I think)

--- gsflow/_RunModel_tr.bat
+++ gsflow/_RunModel_tr.bat
@@ -32,19 +32,6 @@ cd    ..\..\..\
 REM  Call model runfile
 MF_NWT.exe alameda_lower_tr.nam
 
-REM  Perform post-processing routine #1
-cd    .\output\modflow\subbasin_runoff\
-call  get_runoff.exe
-cd    ..\
-
-REM  Peel-out GWET from .lst file for multiplying by 365
-call  inschek.exe  alameda.lst.ins alameda.lst
-
-REM  Multiply GWET from .lst file by 365 for comparing to derived observation value
-call  obs2obs.exe  obs2obs_GWET.in obs2obs_GWET.out
-
-call  obs2obs.exe  obs2obs.in obs2obs.out
-cd    ..\..\
 
 
 
