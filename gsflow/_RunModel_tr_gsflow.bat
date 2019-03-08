beb7b3fa5787b2034475f01b544dc7a6d322b69a
GEOG-LARSEN-WIN\saalem.adera <saalem.adera@gmail.com>
2019-03-08 12:07:24 -0800

    created a _RunModel batch file for gsflow runs

---
+++ gsflow/_RunModel_tr_gsflow.bat
@@ -0,0 +1,41 @@
+REM  Preprocessing routines
+
+REM  Interpolate Kh, SurfK (all contained in one batch routine)
+cd    .\input\modflow_lower\pest\
+call  00_MF_Call_Array_Build.bat
+cd    ..\..\..\
+
+REM  Reformat the LAK bedleakance arrays from a single column to array format
+cd    .\input\modflow_lower\lak_support\
+call  ReformARRAY.exe < RefArr_In_Lay1.txt
+call  ReformARRAY.exe < RefArr_In_Lay2.txt
+call  ReformARRAY.exe < RefArr_In_Lay3.txt
+cd    ..\..\..\
+
+REM  Reformat the FINF & pET scaling arrays from a single column to array format
+cd    .\input\modflow_lower\uzf_support\
+call  ReformARRAY.exe < RefArr_In_scale_finf.txt
+call  ReformARRAY.exe < RefArr_In_scale_pET.txt
+
+REM  Also, reformat the surfK array from a single column to array format
+call  ReformARRAY.exe < RefArr_In_surfK.txt
+
+REM  Scale finf by subasin (original finf array multiplied by scaling array)
+call  twoarray.exe    < twoarray_finf.in
+call  ReformARRAY.exe < RefArr_In_finf.txt
+
+REM  Scale pET by subasin (original pET array multiplied by scaling array)
+call  twoarray.exe    < twoarray_pET.in
+call  ReformARRAY.exe < RefArr_In_pET.txt
+cd    ..\..\..\
+
+REM  Call model runfile
+gsflow.exe alameda_lower_gsflow.control
+
+
+
+
+
+
+
+
